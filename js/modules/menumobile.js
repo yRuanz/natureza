3085e3d39fc54f86f9ee8fe938184cac6ee7ff93
yRuanz <ruanhenriquesenai@gmail.com>
2023-10-16 20:12:11 -0300

    commit

--- js/modules/menumobile.js
+++ js/modules/menumobile.js
@@ -13,3 +13,20 @@ export default function menuMobileInit() {
         toggle.addEventListener('click', toggleMenu);
     }
 }
+
+const form = document.querySelector('.form')
+const dados = {}
+
+function pegarValorForm(event){
+    dados[event.target.name] = event.target.value
+    console.log(dados)
+}
+
+
+
+form.addEventListener('change', pegarValorForm)
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    localStorage.setItem(dados.email, JSON.stringify(dados))
+    alert("Dados salvos com sucesso")
+})
